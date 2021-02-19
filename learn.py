51f1d93ca4f97a7109ec166736f694c85bcbc640
Mick <bear@bear.ru>
2021-02-19 19:01:36 +0300

    one

---
+++ learn.py
@@ -0,0 +1,24 @@
+#task 1
+
+#questionBirth = int(input('В каком году вы родились?'))
+#willAge = int(input('К какому году нужно посчитать возраст?'))
+
+#result = willAge - questionBirth
+
+#print('В 2040г вам будет: ',result)
+
+
+
+
+# task 2
+
+
+human1 = int(input('Сколько вы получаете?,human1'))
+human2 = int(input('Сколько вы получаете?,human2'))
+human3 = int(input('Сколько вы получаете?,human3'))
+
+expenses = (human1 + human2 + human3) / 3
+total = expenses / 30
+
+print(total)
+
